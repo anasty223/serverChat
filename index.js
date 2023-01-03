2142ec235e8f336b8c5189094eb506c291a9994f
Anastasya <anasty22@gmail.com>
2023-01-03 21:32:19 +0200

    update

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5000/",
-    methods: ["GET", "POST"],
-  },
+    origin: 'http://localhost:5000/',
+        methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Credentials'],
+        withCredentials: true
 });
 
 io.on("connection", (socket) => {
