73cc2c01ce7477c0fa3e01092e91b164b02a951e
Anastasya <anasty22@gmail.com>
2023-01-03 20:22:10 +0200

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5000/",
     methods: ["GET", "POST"],
   },
 });
@@ -33,6 +33,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
+server.listen(5000, () => {
   console.log("SERVER RUNNING");
 });
