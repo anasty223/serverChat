e4aec25b1c1061b1cb3acd9503c7618b2c96cca5
Anastasya <anasty22@gmail.com>
2023-01-04 12:34:12 +0200

    upd

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://serverchat-mxy7.onrender.com",
+    origin: "https://realtimchattrunova.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
